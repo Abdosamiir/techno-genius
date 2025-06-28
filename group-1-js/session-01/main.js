65649c197c1b2854399f7df67b42ea5b61d37758
abdosamiir <barakatabdo82@gmail.com>
2025-06-28 15:51:48 +0300

    Add initial HTML, CSS, and JavaScript files for session one

---
+++ group-1-js/session-01/main.js
@@ -0,0 +1,41 @@
+// alert("welcome"); // built in function
+// declare variable
+// data types 7
+//take decision
+
+// var str = "haya";
+// var str = 6;
+
+// const x = undefined;
+
+// const str = "haya ";
+// const str = 56; // cant reassign value
+
+// console.log(5 - "6"); //55
+
+// let x = 5; // number
+// console.log(x);
+// var str = "abdo"; // string
+// var bool = true; // boolean
+
+// var y;
+
+// console.log(y);
+
+// undefined
+// null
+
+console.log(315 - "5");
+
+const num = 50;
+const ageJonas = num - 30;
+// const ageSarah = num - 10;
+
+// console.log(ageJonas > ageSarah); //20 > 40
+console.log(`my age is ${ageJonas}`);
+
+// if (ageJonas > 19) {
+//   console.log("iam 20 years old ");
+// } else {
+//   console.log("i smaller than 20 years old");
+// }
