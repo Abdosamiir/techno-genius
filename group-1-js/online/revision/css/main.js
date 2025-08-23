0864032a380cd1ca1ab50d753eb5a15004cb51a2
abdosamiir <barakatabdo82@gmail.com>
2025-08-23 14:21:02 +0300

    Add responsive hamburger menu and styling for header and navigation
    - Implemented a pure CSS hamburger menu for mobile view.
    - Styled header with gradient text and responsive adjustments.
    - Enhanced navigation with hover effects and mobile-friendly layout.
    - Added transitions for smooth menu appearance and disappearance.

---
+++ group-1-js/online/revision/css/main.js
@@ -0,0 +1,24 @@
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(sum(5, 8)); //13
+
+// if (age > 18) {
+//   console.log("teenager");
+// } else {
+//   console.log("he is still young");
+// }
+
+// age > 18 ? console.log("teenager") : console.log("he is still young");
+
+// const sum = () => {};
+// sum();
+
+// premitive data type
+/*number str bool undefind null bigint */
+// non-primitive
+/* object array is object */
+/*  array and null  is object */
+
+console.log(typeof null);
