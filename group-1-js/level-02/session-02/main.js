d126b60f13524679dab9092a1d9772e7576d09b3
abdosamiir <barakatabdo82@gmail.com>
2025-09-05 20:27:31 +0300

    Add HTML and JavaScript files for session 02, including dynamic content manipulation and styling examples.

---
+++ group-1-js/level-02/session-02/main.js
@@ -0,0 +1,25 @@
+//declaration
+
+function changeTxt() {
+  const para = document.createElement("p");
+  para.textContent = "this is new pargraph";
+  document.getElementById("demo").innerHTML = `Hello JavaScript! ${5 + 6}`;
+  document.body.appendChild(para);
+}
+
+//expression
+// const changeTxt = () => {
+//   document.getElementById("demo").style.backgroundColor = "#00ff87";
+//   document.getElementById("demo").style.fontSize = "35px";
+// };
+
+const div = document.createElement("div");
+div.innerText = "hello haya";
+console.log(div);
+div.style.width = "fit-content";
+div.style.backgroundColor = "red";
+div.style.padding = "10px";
+div.style.border = "5px solid blue";
+div.style.marginTop = "15px";
+
+document.body.appendChild(div);
