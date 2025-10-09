a367b6d96994ee9b953f1e29ce60d1d3bd65bff6
abdosamiir <barakatabdo82@gmail.com>
2025-10-09 13:20:19 +0300

    Add initial HTML structure and CSS styling for layout

---
+++ group-1-js/level-03/session-01/main.js
@@ -0,0 +1,70 @@
+// const breeds = [
+//   {
+//     breed: 'German Shepherd',
+//     averageWeight: 32,
+//     activities: ['fetch', 'swimming'],
+//   },
+//   {
+//     breed: 'Dalmatian',
+//     averageWeight: 24,
+//     activities: ['running', 'fetch', 'agility'],
+//   },
+//   {
+//     breed: 'Labrador',
+//     averageWeight: 28,
+//     activities: ['swimming', 'fetch'],
+//   },
+//   {
+//     breed: 'Beagle',
+//     averageWeight: 12,
+//     activities: ['digging', 'fetch'],
+//   },
+//   {
+//     breed: 'Husky',
+//     averageWeight: 26,
+//     activities: ['running', 'agility', 'swimming'],
+//   },
+//   {
+//     breed: 'Bulldog',
+//     averageWeight: 36,
+//     activities: ['sleeping'],
+//   },
+//   {
+//     breed: 'Poodle',
+//     averageWeight: 18,
+//     activities: ['agility', 'fetch'],
+//   },
+// ];
+
+// const huskyWeight = breeds.find(breed => breed.breed === 'Husky').averageWeight;
+// console.log(huskyWeight);
+
+// const dogBothActivities = breeds.find(
+//   breed =>
+//     breed.activities.includes('running') && breed.activities.includes('fetch')
+// ).breed;
+
+// console.log(dogBothActivities);
+
+// const allActivities = breeds.flatMap(breed => breed.activities);
+// console.log(allActivities);
+
+// const uniqueActivities = [...new Set(allActivities)];
+// console.log(uniqueActivities);
+
+// const swimmingAdjacent = breeds
+//   .filter(breed => breed.activities.includes('swimming'))
+//   .flatMap(breed => breed.activities)
+//   .filter(activity => activity !== 'swimming');
+
+// console.log([...new Set(swimmingAdjacent)]);
+
+// console.log(breeds.every(breed => breed.averageWeight > 10));
+// console.log(breeds.some(breed => breed.activities.length >= 3));
+
+// const fetchWeights = breeds
+//   .filter(breed => breed.activities.includes('fetch'))
+//   .map(breed => breed.averageWeight);
+// const heaviestFetchBreed = Math.max(...fetchWeights);
+// console.log(fetchWeights);
+// console.log(heaviestFetchBreed);
