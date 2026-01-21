c3cd64f445b29670d6589df381be14360a7d0166
abdosamiir <barakatabdo82@gmail.com>
2026-01-21 20:31:07 +0200

    Add initial HTML, CSS, and JavaScript files for project setup

---
+++ online/JS/level-01/session-01/main.js
@@ -0,0 +1,20 @@
+// this is a comment
+
+/* this is multi-line comment */
+
+/* data type
+1. string 
+2. number
+3. boolean => true or false 
+4. object
+5. undefind
+6. null
+*/
+// javascript run line by line 
+// syntax
+var hisName = "mohamed"; // string
+var x = 1; // number
+var bool = true; // boolean 
+
+console.log(x);
+console.log(hisName);
