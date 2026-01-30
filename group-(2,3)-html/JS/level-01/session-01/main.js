63e9a9104bb1517f54b9b18521f61ab81ea7e52b
abdosamiir <barakatabdo82@gmail.com>
2026-01-30 14:50:26 +0200

    Add initial HTML, CSS, and JavaScript files for project setup

---
+++ group-(2,3)-html/JS/level-01/session-01/main.js
@@ -0,0 +1,21 @@
+var z = 4;
+var myName = "abdo";
+
+console.log(typeof z); // detect a data type
+
+if (z == 5) {
+  console.log("its true");
+} else {
+  console.log("x not equal 5");
+}
+
+function sum(x, y) {
+  return x + y;
+}
+console.log(sum(5, 6));
+
+// this is a comment 
+
+/*
+this is a multipul comments 
+*/
