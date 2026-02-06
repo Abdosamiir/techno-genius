058d8d34959d56520bfc4cfc4fc3906ff00f1eda
abdosamiir <barakatabdo82@gmail.com>
2026-02-06 20:40:07 +0200

    Add initial HTML structure and Tailwind CSS integration
    - Created index.html with a basic layout including a header and a card component.
    - Added main.css file with Tailwind CSS styles for layout and design.
    - Included style.css to import Tailwind CSS for styling.

---
+++ group-(2,3)-html/JS/level-01/session-02/main.js
@@ -0,0 +1,56 @@
+// console.log(x);
+
+// let x = 5; // initialze
+
+// const myName = "abdo"; // str
+
+// myName = 5; // reassign  // number
+
+// console.log(myName);
+
+// lastName = "samir"; avoid doing this shit
+
+// console.log(lastName);
+
+const dateNow = 2026;
+const myAge = dateNow - 2003;
+console.log(myAge);
+
+const city = "mansoura";
+console.log(typeof myAge);
+
+const concat = myAge + " " + city;
+
+console.log(concat);
+
+let x = 1;
+x = x + 2;
+x *= 2;
+
+x++;
+x--;
+
+console.log(x);
+
+console.log(x === "6"); // check value and type
+console.log(x == "6"); // check value only
+
+if (x === 6) {
+  console.log(" hello js ");
+} else {
+  console.log("this is a data type string ! ");
+}
+
+// function checkValu(word) {
+//   console.log(typeof word);
+// }
+
+// checkValu(5);
+
+// iam abdo ,a 23 years old
+
+const myName = "abdo";
+
+console.log("iam" + " " + myName + " ,a" + " " + myAge + " " + "years old");
+//  Template Literals
+console.log(`iam ${myName} ,a ${myAge} years old `); // 2015
