60de98c43223e90963b9a24a000373dd85c26767
abdosamiir <barakatabdo82@gmail.com>
2026-07-11 17:55:22 +0300

    Add new HTML and CSS files for session 07, including structure and styling for shapes

---
+++ new-js/main.js
@@ -0,0 +1,32 @@
+console.log(x);
+var x = 5;
+
+var y = 6;
+console.log(x < y);
+console.log(typeof x);
+
+// data types
+
+/*
+
+-- string
+-- number 
+-- boolean
+-- object * array is object in js
+-- null 
+-- undefiend
+
+*/
+
+var obj = {
+  // key : value;
+  fristName: "abdo",
+};
+
+var arr = [1, 2, 1];
+console.log(typeof obj);
+console.log(typeof arr);
+
+// declaration
+
+// var - let - const
